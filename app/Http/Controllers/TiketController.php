deb4af4d96fc11a02ee1942f798fc282103cefe9
aainsomnia <auliadika26@gmail.com>
2023-03-28 15:51:39 +0700

    updates

--- app/Http/Controllers/TiketController.php
+++ app/Http/Controllers/TiketController.php
@@ -114,14 +114,9 @@ public function tiket($year = null, $month = null)
                     $list_item->name = $item_name;
                     // Tambahan FLT Export List
                     $list_item->number = 'N'.date('d/m/y').'/'.$item->id;
-                    $list_item->unit = $item->unit;
-                    $list_item->description = $request->list_description[$key_name];
-                    $list_item->price = $request->list_price[$key_name];
-                    $list_item->qty = $request->list_qty[$key_name];
                     $list_item->boarding_name = $request->list_boarding[$key_name];
                     $list_item->boarding_ktp = $request->list_ktp[$key_name];
                     $list_item->boarding_phone = $request->list_phone[$key_name];
-                    $list_item->jumlah = intval($request->list_price[$key_name]) * intval($request->list_qty[$key_name]);
                     $list_item->save();
                 }
             }
