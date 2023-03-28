e0d90bd5954efe835b5dc48e1b1035f90a39379f
aainsomnia <88727611+aainsomnia@users.noreply.github.com>
2023-03-28 21:50:21 +0700

    dongo update
    update

--- app/Http/Controllers/TiketController.php
+++ app/Http/Controllers/TiketController.php
@@ -87,7 +87,7 @@ public function tiket($year = null, $month = null)
             $item = new Ticket;
             $item->owner_id = $unit->owner_id;
             $item->unit = $unit->full_number;
-            $item->linstasan = $unit->verification;
+            $item->lintasan = $unit->verification;
             $item->request_name = $request->request_name;
             $item->request_date = date('Y-m-d H:i:s', strtotime($request->request_date));
             $item->received_date = date('Y-m-d H:i:s', strtotime($request->received_date));
