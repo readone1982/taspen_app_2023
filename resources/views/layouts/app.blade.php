c17d7ac9837b2bb941f640981a672d936bad4e86
aainsomnia <88727611+aainsomnia@users.noreply.github.com>
2023-03-28 18:09:59 +0700

    updatw
    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,7 +64,7 @@
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">
-                    <img class="brand-logo" src="{{ asset('app-assets') }}/images/taspen.png" alt="Taspen" width="100" height="42" class="d-inline-block align-text-top">
+                    <img class="brand-logo" src="{{ asset('app-assets') }}/images/bumn.png" alt="Taspen" width="100" height="42" class="d-inline-block align-text-top">
                   </a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
