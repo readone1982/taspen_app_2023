2055a270c9c21a5a2d933089aadfcabc4842da7e
aainsomnia <88727611+aainsomnia@users.noreply.github.com>
2023-03-28 20:38:43 +0700

    updates
    updates

--- app/Http/Controllers/TiketController.php
+++ app/Http/Controllers/TiketController.php
@@ -248,7 +248,7 @@ public function tiket($year = null, $month = null)
 
         $pdf = PDF::loadview('tenant.tiket.pdf', $data)
         ->setOptions(['defaultFont' => 'calibri', 'isHtml5ParserEnabled' => true, 'isRemoteEnabled' => true, 'chroot' => public_path('app-assets/images')])
-        ->setPaper('A4','potrait');
+        ->setPaper('Legal','potrait');
         return $pdf->stream($tiket->number.'.pdf');
     }
 }
--- resources/views/tenant/tiket/pdf.blade.php
+++ resources/views/tenant/tiket/pdf.blade.php
@@ -196,6 +196,10 @@
 <br>
 <br>
 <br>
+<br>
+<br>
+<br>
+<br>
 
 <!-- test -->
         <table style="width: 100%;padding: 0;margin: 0; background-color: #f2f2f28b;">
