86dd6673ebddb60b016bc6b332866de992769d54
aainsomnia <auliadika26@gmail.com>
2023-03-29 09:56:54 +0700

    check

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -164,7 +164,7 @@
                                             <h3 class="info"></h3>
                                             <?php
                                             if ($bus4 > "5") {
-                                                echo '<h6 style="font-weight: bold;color: rgb(128, 0, 0);">' "Kuota" . $bus4 . '</h6>';
+                                                echo '<h6 style="font-weight: bold;color: rgb(128, 0, 0);"> Kuota '  . $bus4 . '</h6>';
                                             } else {
                                                 echo '<h6 style="font-weight: bold;color: rgb(1, 0, 0);">' . $bus4 . '</h6>';
                                             }
