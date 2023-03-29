29c0daaedad617ef9338a26150c0ff34db0e99f1
aainsomnia <auliadika26@gmail.com>
2023-03-29 09:33:56 +0700

    updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -163,8 +163,11 @@
                                         <div class="media-body text-left">
                                             <h3 class="info"></h3>
                                             @php
-                                            if($bus4) > 35 {
-                                                echo '<span style="font-weight: bold;color: rgb(128, 0, 0);">>BUS 4 - SOLO/YOGYAKARTA VIA UTARA Kuota Test Warna {{$bus4}} /40 </span>';
+                                            if($bus4) == 35 {
+                                                echo '<span style="font-weight: bold;color: rgb(0, 0, 0);">>BUS 4 - SOLO/YOGYAKARTA VIA UTARA Kuota Test Warna {{$bus4}} /40 </span>';
+                                            }
+                                            elseif($bus4) > 35 {
+                                                echo '<span style="font-weight: bold;color: rgb(243, 16, 16);">>BUS 4 - SOLO/YOGYAKARTA VIA UTARA Kuota Test Warna {{$bus4}} /40 </span>';
                                             }
                                             @endphp
                                             <h6>BUS 4 - SOLO/YOGYAKARTA VIA UTARA Kuota {{$bus4}} / 40</h6>
